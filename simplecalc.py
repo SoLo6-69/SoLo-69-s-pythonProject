8f7dd4fa52da91ff8a2738eb14c175fd0d19623e
Solo-69 <126795212+SoLo6-69@users.noreply.github.com>
2023-03-03 11:46:06 +0530

    Add files via upload
    This is a simple calculator

---
+++ simplecalc.py
@@ -0,0 +1,30 @@
+a = 5
+b = 7
+ans = a+b
+
+c = 10
+d = 8
+ans1 = c-d
+
+e = 77
+f = 88
+ans2 = e*f
+
+g = 6
+h = 10
+ans3 = g/h
+
+i = 6
+j = 4 
+ans4 = i**j
+
+k = 3
+l = 2
+ans5 = k//l
+
+print("1.Answer of 5+7 is "+str(ans))
+print("2.Answer of 10-8 is "+str(ans1))
+print("3.Answer of 77x88 is "+str(ans2))
+print("4.Answer of 6/10 is "+str(ans3))
+print("5.Answer of 6**4 is "+str(ans4))
+print("6.Answer of 3//2 is "+str(ans5))
